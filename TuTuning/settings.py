fd62b875ada13d8a823d89827cb16a9276aa9a32
ACEGAZ <garylowles@gmail.com>
2023-03-22 21:39:48 +0000

    edited heroku allowed hosts for deployment

--- TuTuning/settings.py
+++ TuTuning/settings.py
@@ -31,7 +31,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 
 ALLOWED_HOSTS = ['localhost']
-CSRF_TRUSTED_ORIGINS = ["https://8000-acegaz-tutuning-xn4rsj1e4uv.ws-eu90.gitpod.io", 'tu-tuning.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ["https://8000-acegaz-tutuning-xn4rsj1e4uv.ws-eu90.gitpod.io", 'TU-Tuning.herokuapp.com']
 
 
 # Application definition
