91d4e793464943097eb035a56ae05f7101f427cc
ACEGAZ <garylowles@gmail.com>
2023-03-22 22:37:42 +0000

    edit about us page, add email host password

--- TuTuning/settings.py
+++ TuTuning/settings.py
@@ -31,7 +31,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 
 ALLOWED_HOSTS = ['localhost', '.herokuapp.com']
-CSRF_TRUSTED_ORIGINS = ["https://8000-acegaz-tutuning-xn4rsj1e4uv.ws-eu90.gitpod.io"]
+CSRF_TRUSTED_ORIGINS = ["https://8000-acegaz-tutuning-xn4rsj1e4uv.ws-eu92.gitpod.io"]
 
 
 # Application definition
