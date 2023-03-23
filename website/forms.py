0cb443926b985c879c6ea4c750b8a78a22d025c5
ACEGAZ <garylowles@gmail.com>
2023-03-23 20:28:50 +0000

    edited home page to stack col's on mobile devices

--- website/forms.py
+++ website/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 
 class ContactForm(forms.Form):
+    """ contact form for contact page"""
     full_name = forms.CharField(max_length=50)
     phone_number = forms.CharField(max_length=20)
     Registration = forms.CharField(max_length=20)
